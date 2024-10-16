2d46d2cd747c3b2c52d3deb48c1845366d7aa637
gracerhpotter <114320346+gracerhpotter@users.noreply.github.com>
2024-10-16 10:18:15 -0700

    Initial Commit

---
+++ Wordier/WordierApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordierApp.swift
+//  Wordier
+//
+//  Created by Grace Potter on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WordierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordierTests/WordierTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordierTests.swift
+//  WordierTests
+//
+//  Created by Grace Potter on 10/16/24.
+//
+
+import Testing
+@testable import Wordier
+
+struct WordierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
